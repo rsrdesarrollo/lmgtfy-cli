126393050ea082b27652f8237d56fe20dfb0ee76
rsrdesarrollo <rsrdesarrollo@gmail.com>
2017-02-07 11:56:52 +0100

    prepare for PyPi

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
 setup(
+    author='Raul Sampedro',
+    author_email='rsrdesarrollo@gmail.com',
     name='lmgtfy',
     version='1.0.1',
-    packages=[''],
+    packages=['lmgtfy'],
     url='https://github.com/rsrdesarrollo/lmgtfy-cli',
     license='GPLv3',
-    author='rsrdesarrollo',
-    author_email='rsrdesarrollo@gmail.com',
     description='Let me Google that for you CLI link generator.',
     scripts=['src/lmgtfy'],
+    keywords=['cli', 'google', 'search', 'generator'],
     requires=['clipboard']
 )
