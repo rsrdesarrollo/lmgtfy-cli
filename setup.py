e50ea845d3fb72f12887529d591006e5fe747371
rsrdesarrollo <rsrdesarrollo@gmail.com>
2017-02-07 12:00:06 +0100

    prepare for PyPi readme

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
+VERSION='1.0.1'
+
 setup(
     author='Raul Sampedro',
     author_email='rsrdesarrollo@gmail.com',
     name='lmgtfy',
-    version='1.0.1',
+    version=VERSION,
     packages=['lmgtfy'],
-    url='https://github.com/rsrdesarrollo/lmgtfy-cli',
+    url='https://github.com/rsrdesarrollo/lmgtfy-cli/tarball/{}'.format(VERSION),
     license='GPLv3',
     description='Let me Google that for you CLI link generator.',
     scripts=['src/lmgtfy'],
